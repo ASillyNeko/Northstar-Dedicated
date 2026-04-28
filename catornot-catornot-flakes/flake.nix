7337aef4a9f5e4491f962f64918888742fc8ade4
ASillyNeko <ASillyNeko@asillyneko.dev>
2026-04-28 14:58:38 -0700

    `nixos-unstable`

--- catornot-catornot-flakes/flake.nix
+++ catornot-catornot-flakes/flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
     };
--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     catornot-flakes.url= "path:./catornot-catornot-flakes";
   };
 
